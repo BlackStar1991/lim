f4357c88115304d319612eb49f143b37302c0b59
DELL-PC\Andry Zirka <zirkaandry@gmil.com>
2017-07-11 18:11:43 +0300

    first step

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //	LENG
-    $('.leng-item').click(function() {
+    $('.leng-item').click(function () {
         $(this).closest('.leng-list__item').addClass('active').siblings().removeClass('active');
     });
 
@@ -16,15 +16,15 @@ $(document).ready(function() {
         slidesToScroll: 1,
         centerMode: true,
         slidesToShow: 1,
-		responsive: [{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				infinite: true
-			}
-		}]
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: false,
+                infinite: true
+            }
+        }]
     });
     //	BRaND-SLIDER	
     $(".owl-carousel-2").slick({
@@ -43,10 +43,8 @@ $(document).ready(function() {
     });
 
 
-
-
     //	ancor
-	jQuery(".link-top").click(function() {
+    jQuery(".link-top").click(function () {
         var elementClick = jQuery(this).attr("href");
         var destination = jQuery(elementClick).offset().top;
         jQuery("html, body").animate({
@@ -56,33 +54,33 @@ $(document).ready(function() {
 
         return false;
     });
-	
-//	MENU
-	$('.header-page').on('click', '.open-menu-min', function(){
-		$('.header-page').toggleClass('open-sub-menu');
-		$('.header-page').removeClass('open-main-menu');
-	});
-	
-	$('.header-page').on('click', '.open-menu-main', function(){
-		$('.header-page').removeClass('open-sub-menu');
-		$('.header-page').toggleClass('open-main-menu');
-	});
-	
-//	search
-	$('.header-page').on('click', '.mobile-ico__search', function(){
-		$('.header-page').removeClass('open-sub-menu');
-		$('.header-page').toggleClass('open-search');
-		$('.header-page').removeClass('open-main-menu');
-	});
 
-	$('.header-page').on('click', '.close-search', function(){
-		$('.header-page').toggleClass('open-search');
-	});
-	
+//	MENU
+    $('.header-page').on('click', '.open-menu-min', function () {
+        $('.header-page').toggleClass('open-sub-menu');
+        $('.header-page').removeClass('open-main-menu');
+    });
+
+    $('.header-page').on('click', '.open-menu-main', function () {
+        $('.header-page').removeClass('open-sub-menu');
+        $('.header-page').toggleClass('open-main-menu');
+    });
+
+//	search
+    $('.header-page').on('click', '.mobile-ico__search', function () {
+        $('.header-page').removeClass('open-sub-menu');
+        $('.header-page').toggleClass('open-search');
+        $('.header-page').removeClass('open-main-menu');
+    });
+
+    $('.header-page').on('click', '.close-search', function () {
+        $('.header-page').toggleClass('open-search');
+    });
+
 //	footer
-	$('.footer-page').on('click', '.h3-title', function(){
-		$(this).closest('.foot-info-list__title').toggleClass('active');
-	});
+    $('.footer-page').on('click', '.h3-title', function () {
+        $(this).closest('.foot-info-list__title').toggleClass('active');
+    });
 });
 
 
